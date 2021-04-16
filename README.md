0683904fbc42bbf36efdc3cf785bf09b5c6ed82e
Chris Verringer <verringerc@gmail.com>
2021-04-16 01:36:58 +0100

    Added groups functionality

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 Adds an endpoint to the XenForo 2 API to grab a full list of staff members from the board.
 
+## Group Endpoint
+
 Endpoint can be accessed at /api/users/group with a valid XF-Api-Key + id parameter (http://dev.xenforo/api/users/group?id=4 for Administrator)
+
+This will be default override the groups endpoint if set.
+
+## Groups Endpoint
+
+Endpoint can be accessed at /api/users/group with a valid XF-Api-Key + comma separated parameter called ids (http://dev.xenforo/api/users/group?ids=1,2,3)
+
+
+## Also
 
 Huge thanks to Sim on XenForo forums for his release of 'Find User by Criteria' for the XenForo API, which pointed me in the right direction with this. Recommend also installing his addon if you're starting to use the XenForo API: https://xenforo.com/community/resources/api-endpoint-find-a-user-by-criteria.7876/
--- XF/Api/Controller/Users.php
+++ XF/Api/Controller/Users.php
@@ -17,23 +17,46 @@ class Users extends XFCP_Users
 	public function actionGetGroup(ParameterBag $params)
 	{
 
-		$group = $this->filter('id', 'int');
+		// Group ID
+		if ($this->filter('id', 'int')) {
+			$group = $this->filter('id', 'int');
+		}
 
-		/** @var \XF\Finder\User $finder */
+		// Groups STR CSL
+		elseif($this->filter('ids', 'str')) {
+			$groups = $this->filter('ids', 'str');
+		}
+
 		$finder = $this->finder('XF:User');
 
 		$user = null;
 
-		// Prep to add $groups in the future via CSL
 		if(isset($group)) {
 			$column = $finder->columnSqlName('secondary_group_ids');
-
+			
 			$user = $finder->with('api')->whereSql("FIND_IN_SET(". $group .", ". $finder->columnSqlName('secondary_group_ids').") OR ". $finder->columnSqlName('user_group_id'). " = ". $group ."");
 
 			$users = array();
 
 			foreach($user as $user) {
 				$users [] = $user;
+			}
+		}
+
+		if(isset($groups)) {
+			$groups_arr = array_unique(explode(',', $groups));
+			$users = array();
+			$x = 0;
+			foreach($groups_arr as $value) {
+				$finder = \XF::finder('XF:User');
+				$user = $finder->whereSql("FIND_IN_SET(". $value .", ". $finder->columnSqlName('secondary_group_ids').") OR ". $finder->columnSqlName('user_group_id'). " = ". $value ."");
+				$users_arr = $user->fetch();
+				$users[$x]['usergroup_id'] = $value;
+				foreach ($users_arr as $key => $user)
+				{
+					$users[$x][] = $user;
+				}
+				$x++;
 			}
 		}
 
